9f2692b5c280a0cbd0e1780995a4456675d7e5bc
Leandro Humberto <leandrohumberto@gmail.com>
2018-03-04 19:58:51 -0300

    Section 02 - ASP.NET MVC Fundamentals.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        readonly IList<Customer> _customers = new List<Customer>
+        {
+            new Customer{ Id = 1, Name = "John Smith" },
+            new Customer{ Id = 2, Name = "Mary Williams" }
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _customers.FirstOrDefault(c => c.Id == id);
+            return View(customer);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,33 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1" },
+                new Customer { Name = "Customer2" }
+            };
+            var viewModel = new RandomViewModel { Movie = movie, Customers = customers };
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            return View(new List<Movie>
+            {
+                new Movie { Name = "Shrek" },
+                new Movie { Name = "Wall-e" },
+            });
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, byte month)
+        {
+            return Content($"{year}/{month}");
         }
     }
 }
